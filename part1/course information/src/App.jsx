d21e9a316ea130b973184448cdbd5436b7a9e96a
SharminNusrat <bsse1441@iit.du.ac.bd>
2025-03-12 00:08:47 +0600

    Task 1.4

--- part1/course information/src/App.jsx
+++ part1/course information/src/App.jsx
@@ -5,6 +5,7 @@ const Header = (props) => {
 }
 
 const Part = (props) => {
+  console.log(props);
   return (
     <p>{props.part} {props.exercise}</p>
   )
@@ -22,30 +23,32 @@ const Content = (props) => {
 
 const Total = (props) => {
   return (
-    <p>Number of exercises {props.exercises}</p>
+    <p>Number of exercises {props.exercises[0].exercises + props.exercises[1].exercises + props.exercises[2].exercises}</p>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content contents={[part1, part2, part3]} />
-      <Total exercises={part1.exercises + part2.exercises + part3.exercises} />
+      <Content contents={parts} />
+      <Total exercises={parts} />
     </div>
   )
 }
