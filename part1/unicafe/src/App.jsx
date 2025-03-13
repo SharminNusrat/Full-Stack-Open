fa8a1821853a2fc81dd7db14016b0a7ed274ec54
SharminNusrat <bsse1441@iit.du.ac.bd>
2025-03-13 17:07:05 +0600

    Task 1.6 unicafe step1

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+
+const Display = (props) => {
+  return (
+    <h1>{props.text}</h1>
+  )
+}
+
+const Button = ({onClick, text}) => {
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
+const Feedback = ({type, count}) => {
+  return (
+    <div>{type} {count}</div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodFeedback = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralFeedback = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadFeedback = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <Display text="give feedback" />
+      <Button onClick={handleGoodFeedback} text="good" />
+      <Button onClick={handleNeutralFeedback} text="neutral" />
+      <Button onClick={handleBadFeedback} text="bad" />
+      <Display text="statistics" />
+      <Feedback type="good" count={good}/>
+      <Feedback type="neutral" count={neutral}/>
+      <Feedback type="bad" count={bad}/>
+    </div>
+  )
+}
+
+export default App
