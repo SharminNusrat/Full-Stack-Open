c26748a389d56f7c75ffee57f520401eb1f9df83
SharminNusrat <bsse1441@iit.du.ac.bd>
2025-03-19 20:22:29 +0600

    Task 2.11 phonebook step6

--- part2/phone book/src/App.jsx
+++ part2/phone book/src/App.jsx
@@ -1,19 +1,26 @@
-import { useState } from "react"
+import { useEffect,useState } from "react"
+import axios from "axios"
 import Filter from "./components/Filter"
 import PersonForm from "./components/PersonForm"
 import Persons from "./components/Persons"
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearchValue, setNewSearchValue] = useState('')
   const [filteredPersons, setFilteredPersons] = useState(persons)
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+        setFilteredPersons(response.data.filter(person => 
+          person.name.toLowerCase().includes(newSearchValue.toLowerCase())
+        ))
+      })
+  }, [])
 
   const addNewPerson = (event) => {
     event.preventDefault()
