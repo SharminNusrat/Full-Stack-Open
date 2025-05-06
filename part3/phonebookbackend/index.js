c35e808a612a2dde3d289db0257bc4ec139dcc31
SharminNusrat <bsse1441@iit.du.ac.bd>
2025-05-06 06:24:01 +0600

    Task 3.15 phone book and database, step3

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -82,9 +82,13 @@ app.post('/api/persons', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+    Person.findByIdAndDelete(id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => {
+            console.log(error)
+        })
 })
 
 
