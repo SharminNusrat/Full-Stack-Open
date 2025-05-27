a096f2f4a9b631863ce7951608d985a3727d26a9
SharminNusrat <bsse1441@iit.du.ac.bd>
2025-05-27 06:52:26 +0600

    Task 4.2 blog list, step2

--- part4/bloglistbackend/index.js
+++ part4/bloglistbackend/index.js
@@ -1,39 +1,8 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-dotenv.config()
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+const port = config.PORT
+app.listen(port, () => {
+  logger.info(`Server running on port ${port}`)
 })
