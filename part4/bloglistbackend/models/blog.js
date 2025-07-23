52a35d0c920aa44a4a7667413acc2d11163a570e
SharminNusrat <bsse1441@iit.du.ac.bd>
2025-07-23 07:07:44 +0600

    Task 4.17 Blog List Expansion, step5

--- part4/bloglistbackend/models/blog.js
+++ part4/bloglistbackend/models/blog.js
@@ -1,16 +1,22 @@
 const mongoose = require('mongoose')
 
 const blogSchema = mongoose.Schema({
-  title: {type: String, required: true},
+  title: { type: String, required: true },
   author: String,
-  url: {type: String, required: true},
-  likes: {type: Number, default: 0},
+  url: { type: String, required: true },
+  likes: { type: Number, default: 0 },
+  user:
+  {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 blogSchema.set('toJSON', {
   transform: (document, returndedObject) => {
     returndedObject.id = returndedObject._id.toString()
     delete returndedObject._id
+    delete returndedObject.__v
   }
 })
 
